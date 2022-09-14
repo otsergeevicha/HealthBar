3ed532e6a71f7e4871428ed3f3e17ecc3dc91ff5
Engener <otsergeevicha@gmail.com>
2022-09-14 19:32:32 +0300

    3 Step
    3 Step

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -4,39 +4,37 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public event Action<float, float> HealthChanged;
+    public const int Died = 0;
     private float _maxHealth = 100;
     private float _health = 100;
 
-    public void CauseDamage(float damageAmount)
+    public void Damage(float damageAmount)
     {
-        if ( _health > 0 && _health >= damageAmount )
-        {
-            TakeDamage(damageAmount);
-        }
-
-        if ( _health < damageAmount )
-        {
-            _health = 0;
-        }
+        TakeDamage(damageAmount);
     }
 
-    public void CauseHeal(float healAmount)
+    public void Heal(float healAmount)
     {
-        if ( _health != 0 && _health != _maxHealth)
-        {
-            TakeHeal(healAmount);
-        }
+        TakeHeal(healAmount);
     }
 
     private void TakeHeal(float heal)
     {
         _health += heal;
+        _health = CheckingValue();
         HealthChanged?.Invoke(_health, _maxHealth);
     }
 
     private void TakeDamage(float damage)
     {
         _health -= damage;
+        _health = CheckingValue();
         HealthChanged?.Invoke(_health, _maxHealth);
+    }
+
+    private float CheckingValue()
+    {
+        _health = Mathf.Clamp(_health, Died, _maxHealth);
+        return _health;
     }
 }
